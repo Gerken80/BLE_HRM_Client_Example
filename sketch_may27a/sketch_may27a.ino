e08e74f4d5c53982c30daf02d3de9e0ec0431781
Gerken80 <105987881+Gerken80@users.noreply.github.com>
2022-05-27 21:43:15 +1000

    Another BLE Example

--- sketch_may27a/sketch_may27a.ino
+++ sketch_may27a/sketch_may27a.ino
@@ -28,8 +28,8 @@ void loop() {
     Serial.println("Connecting");
     
     while (foundDevice.connected()){
-      Serial.println("Connected");
-      delay(1000); 
+      connectedDeviceDetails(foundDevice);
+      delay(5000); 
     }
     
   }
@@ -37,4 +37,19 @@ void loop() {
   Serial.println("Disconected");
   BLE.scan();
   delay(1000); 
+}
+
+void connectedDeviceDetails(BLEDevice connectedDevice){
+     int i = 0;
+     Serial.print(connectedDevice.localName());
+     Serial.println(" is Connected, Discovering attributes..");
+     connectedDevice.discoverAttributes();
+     int ServiceCount = connectedDevice.serviceCount();
+     Serial.print("Service count is: ");
+     Serial.println(ServiceCount);
+     while (i < ServiceCount){
+      BLEService deviceService = connectedDevice.service(i);
+      Serial.println(deviceService.uuid());
+      i++;
+     }
 }
